de98a1076bdc7a61c2fdefa5ac04e4a121f51e67
benediktstroebl <stroebl@princeton.edu>
2024-06-02 15:25:33 -0400

    added custom pricing for loop

--- script.js
+++ script.js
@@ -10,13 +10,38 @@ $(document).ready(function() {
             var y = [];
             var text = [];
 
+            var gpt4_cost = 0;
+            var gpt3_cost = 0;
+            var reflexion_cost = 0;
+
             data.forEach(item => {
                 var cost = 0;
-                if (item.model === 'GPT-4') {
+                if (item.strategy_renamed === 'GPT-4') {
+                    gpt4_cost = item.mean_prompt_tokens * gpt4PromptPrice + item.mean_completion_tokens * gpt4CompletionPrice;
+                } else if (item.strategy_renamed === 'GPT-3.5') {
+                    gpt3_cost = item.mean_prompt_tokens * gpt3PromptPrice + item.mean_completion_tokens * gpt3CompletionPrice;
+                } else if (item.strategy_renamed === 'Reflexion (GPT-4)') {
+                    reflexion_cost = item.mean_prompt_tokens * gpt4PromptPrice + item.mean_completion_tokens * gpt4CompletionPrice;
+                }
+            });
+
+            data.forEach(item => {
+                var cost = 0;
+                if (item.strategy_renamed === 'LDB (GPT-3.5)') {
+                    cost = gpt3_cost + item.mean_prompt_tokens * gpt3PromptPrice + item.mean_completion_tokens * gpt3CompletionPrice;
+                } else if (item.strategy_renamed === 'LDB (GPT-4)') {
+                    cost = gpt4_cost + item.mean_prompt_tokens * gpt4PromptPrice + item.mean_completion_tokens * gpt4CompletionPrice;
+                } else if (item.strategy_renamed === 'LDB (GPT-4, GPT-3.5)') {
+                    cost = gpt4_cost + item.mean_prompt_tokens * gpt3PromptPrice + item.mean_completion_tokens * gpt3PromptPrice;
+                } else if (item.strategy_renamed === 'LDB (Reflexion, GPT-3.5)') {
+                    cost = reflexion_cost + item.mean_prompt_tokens * gpt3PromptPrice + item.mean_completion_tokens * gpt3PromptPrice;
+                } else if (item.strategy_renamed === 'LDB (Reflexion, GPT-4)') {
+                    cost = reflexion_cost + item.mean_prompt_tokens * gpt4PromptPrice + item.mean_completion_tokens * gpt4CompletionPrice;
+                } else if (item.model === 'GPT-4') {
                     cost = item.mean_prompt_tokens * gpt4PromptPrice + item.mean_completion_tokens * gpt4CompletionPrice;
                 } else if (item.model === 'GPT-3.5') {
                     cost = item.mean_prompt_tokens * gpt3PromptPrice + item.mean_completion_tokens * gpt3CompletionPrice;
-                }
+                } 
                 x.push(cost);
                 y.push(item.mean_accuracy);
                 text.push(item.strategy_renamed + ' (' + item.model + ')');
